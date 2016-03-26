76eb4e07e8f9ae229cc11fde23c137417b91905b
lsl233 <1156021653@qq.com>
2016-03-26 20:41:23 +0800

    添加验证

--- public/js/page/task_1_16_1.js
+++ public/js/page/task_1_16_1.js
@@ -13,6 +13,79 @@
 var aqiData = {};
 
 /**
+ * 验证策略
+ * @type {Object}
+ */
+var strategies = {
+    isNonEmpty: function (value, errorMsg) {
+        if (value === '') {
+            return errorMsg;
+        }
+    },
+
+    minLength: function (value, length, errorMsg) {
+        if (value.length < length) {
+            return errorMsg;
+        }
+    },
+
+    isMobile: function (value, errorMsg) {
+        if (!/(^1[3|5|8][0-9]{9}%)/.test(value)) {
+            return errorMsg;
+        }
+    },
+
+    isChinese: function (value, errorMsg) {
+        if (/[^\u4e00-\u9fa5]/.test(value)) {
+            return errorMsg;
+        }
+    },
+
+    isNumber: function (value, errorMsg) {
+        if (!/^[0-9]*$/.test(value)) {
+            return errorMsg;
+        }
+    }
+}
+
+var Validator = function () {
+    this.cache = [];
+};
+
+Validator.prototype.add = function (value, rules, errorMsg) {
+    // var ary = rule.split(':');
+    // this.cache.push(function () {
+    //     var strategy = ary.shift();
+    //     ary.unshift(value);
+    //     ary.push(errorMsg);
+    //     return strategies[strategy].apply(value, ary);
+    // });
+    var self = this;
+
+    for (var i = 0, rule; rule = rules[i++];) {
+        (function(rule) {
+            var strategyAry = rule.strategy.split(':');
+            var errorMsg = rule.errorMsg;
+
+            self.cache.push(function () {
+                var strategy = strategyAry.shift();
+                strategyAry.unshift(value);
+                strategyAry.push(errorMsg);
+                return strategies[strategy].apply(null, strategyAry);
+            });
+        })(rule)
+    }
+};
+
+Validator.prototype.start = function () {
+    for (var i = 0, validatorFunc; validatorFunc = this.cache[i++];) {
+        var msg = validatorFunc();
+        if (msg) {
+            return msg;
+        }
+    }
+};
+/**
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
  */
@@ -23,16 +96,38 @@ function addAqiData() {
     ctiy = document.getElementById('aqi-city-input').value;
     index = document.getElementById('aqi-value-input').value;
 
-    if (/[^\u4e00-\u9fa5]/.test(ctiy) || !ctiy) {
-        alert('请输入中文');
-        return false;
+    function validataFunc () {
+        var v = new Validator();
+        // v.add(ctiy, 'isChinese', '请输入中文');
+        // v.add(index, 'minLength:10', '请输入数字');
+        v.add(ctiy, [{
+            strategy: 'isChinese',
+            errorMsg: '请填写中文'
+        }, {
+            strategy: 'isNonEmpty',
+            errorMsg: '不能为空'
+        }]);
+
+        v.add(index, [{
+            strategy: 'isNumber',
+            errorMsg: '请填写数字'
+        }, {
+            strategy: 'isNonEmpty',
+            errorMsg: '不能为空'
+        }]);
+
+        var errorMsg = v.start();
+        return errorMsg;
     }
-    if (!/^[0-9]*$/.test(index) || !index) {
-        alert('请输入数字');
+    
+    var err = validataFunc();
+    if (err) {
+        alert(err);
         return false;
     }
 
     aqiData[ctiy] = index;
+    return true;
 }
 
 /**
@@ -59,8 +154,10 @@ function renderAqiList() {
  * 获取用户输入，更新数据，并进行页面呈现的更新
  */
 function addBtnHandle() {
-    addAqiData();
-    renderAqiList();
+    var is = addAqiData();
+    if (is) {
+       renderAqiList(); 
+    }  
 }
 
 /**
